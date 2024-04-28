9acc10a721d52051b2ad2e3fbdd17718c038d00a
Amel RAZINI <amelrazini5@gmail.com>
2024-04-28 21:07:34 +0200

    mlm

--- src/App/slices/userSlice.js
+++ src/App/slices/userSlice.js
@@ -2,7 +2,11 @@ import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 export const userSlice = createSlice({
   name: "user",
-  initialState: JSON.parse(localStorage.getItem("user")),
+  initialState:
+    JSON.parse(localStorage.getItem("user")) ||
+    JSON.parse(
+      '{"user":{"_id":"65fb641023e9bd5beb4be325","email":"amelrazini@gmail.com","password":"azerty","name":"amel","role":"admin","orders":[],"__v":0}}'
+    ),
   reducers: {
     setUser: (state, action) => {
       console.log(state, "state");
@@ -14,6 +18,5 @@ export const userSlice = createSlice({
       return (state = null);
     },
   },
-  
 });
 export const { logout, setUser } = userSlice.actions;
