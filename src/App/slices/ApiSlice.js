97c2b1c811d10a3cc62e428db34065ad38f79aa1
Amel RAZINI <amelrazini5@gmail.com>
2024-04-29 20:43:25 +0200

    fvjn

--- src/App/slices/ApiSlice.js
+++ src/App/slices/ApiSlice.js
@@ -2,7 +2,10 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  // baseQuery: fetchBaseQuery({ baseUrl: import.meta.env._VITE_BACK_END_URL }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://backend-hosting-1.onrender.com",
+  }),
   endpoints: (builder) => ({
     sendEmail: builder.mutation({
       query: (data) => ({
